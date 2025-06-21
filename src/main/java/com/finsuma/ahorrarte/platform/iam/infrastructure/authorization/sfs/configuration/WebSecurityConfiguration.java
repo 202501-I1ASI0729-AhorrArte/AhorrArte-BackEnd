d1ed513509616110972e8f975de6915368914c6f
Andres-0304 <andresfer0304@gmail.com>
2025-06-20 22:31:34 -0500

    deploy

--- src/main/java/com/finsuma/ahorrarte/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/finsuma/ahorrarte/platform/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -101,20 +101,15 @@ public class WebSecurityConfiguration {
         }));
         http.csrf(csrfConfigurer -> csrfConfigurer.disable())
                 .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedRequestHandler))
-                .sessionManagement( customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .sessionManagement( customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(
                                 "/api/v1/authentication/**",
-                                "/api/v1/authentication/sign-in",
-                                "/api/v1/authentication/sign-up", 
-                                "/actuator/**",
-                                "/actuator/health/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/swagger-resources/**",
-                                "/webjars/**",
-                                "/favicon.ico",
-                                "/error").permitAll()
+                                "/webjars/**").permitAll()
                         .anyRequest().authenticated());
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authorizationRequestFilter(), UsernamePasswordAuthenticationFilter.class);
