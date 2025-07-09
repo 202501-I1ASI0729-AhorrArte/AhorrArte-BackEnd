1127173876e210f4b8f1725d01f52eefe31e51ce
Andres-0304 <andresfer0304@gmail.com>
2025-07-09 04:56:38 -0500

    fix: OpenApiConfiguration

--- src/main/java/com/finsuma/ahorrarte/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/finsuma/ahorrarte/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -18,14 +18,14 @@ public class OpenApiConfiguration {
         var openApi = new OpenAPI();
         openApi
                 .info(new Info()
-                        .title("Upecinos AI AhorrArte Platform API")
-                        .description("Upecinos AI AhorrArte Platform application REST API documentation.")
+                        .title("FinSuma AhorrArte Platform API")
+                        .description("FinSuma AhorrArte Platform application REST API documentation.")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("Upecinos AI AhorrArte Platform Wiki Documentation")
-                        .url("https://finsuma-AhorrArte-platform.wiki.github.io/docs"));
+                        .description("FinSuma AhorrArte Platform Wiki Documentation")
+                        .url("https://ahorrarte-backend.onrender.com/swagger-ui/index.html"));
 
         final String securitySchemeName = "bearerAuth";
 
