e6843ad5d6f6dc2439cdd937f4b529c0ebde1ba5
Andres-0304 <andresfer0304@gmail.com>
2025-07-09 02:48:45 -0500

    fix: deploy

--- src/main/java/com/finsuma/ahorrarte/platform/AhorrArtePlatformApplication.java
+++ src/main/java/com/finsuma/ahorrarte/platform/AhorrArtePlatformApplication.java
@@ -2,14 +2,8 @@ package com.finsuma.ahorrarte.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.finsuma.ahorrarte", "com.FinSuma.AhorrArte"})
-@EntityScan(basePackages = {"com.finsuma.ahorrarte", "com.FinSuma.AhorrArte"})
-@EnableJpaRepositories(basePackages = {"com.finsuma.ahorrarte", "com.FinSuma.AhorrArte"})
 public class AhorrArtePlatformApplication {
 
     public static void main(String[] args) {
